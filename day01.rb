f5ed9807defb7e6d3c340f32caa2cb023d226b7c
Víctor González Prieto <victor.gp.dev@gmail.com>
2024-12-02 01:11:27 +0100

    Add solution for day 1 part 2

--- day01.rb
+++ day01.rb
@@ -11,8 +11,29 @@ end
 
 sorted1 = location_ids1.sort
 sorted2 = location_ids2.sort
-pairs = sorted1.zip(sorted2)
 
-total_distance = pairs.inject(0) { |sum, pair| sum + (pair[0] - pair[1]).abs }
+similarity_score = 0
+i = 0
+j = 0
+while sorted1[i] && sorted2[i] do
+  if sorted1[i] < sorted2[j]
+    i += 1
+  elsif sorted1[i] > sorted2[j]
+    j += 1
+  else
+    matching_id = sorted1[i]
+    count2 = 0
+    while sorted2[j] == matching_id do
+      j += 1
+      count2 += 1
+    end
+    count1 = 0
+    while sorted1[i] == matching_id do
+      i += 1
+      count1 += 1
+    end
+    similarity_score += matching_id * count2 * count1
+  end
+end
 
-puts total_distance
+puts similarity_score
