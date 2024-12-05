378bae132608e6f513fa741639c554c2505400cf
Víctor González Prieto <victor.gp.dev@gmail.com>
2024-12-05 16:37:19 +0100

    Add solution for day 3 part 2

--- day03.rb
+++ day03.rb
@@ -4,13 +4,39 @@ MUL_REGEX =  /mul\(\d{1,3},\d{1,3}\)/
 # same regex but captures the variables
 VARS_REGEX = /mul\((\d{1,3}),(\d{1,3})\)/
 
-program_memory = STDIN.read
-valid_instructions = program_memory.scan(MUL_REGEX)
-
-result = valid_instructions.inject(0) do |sum, ins|
-  vars = VARS_REGEX.match(ins).captures
-  x, y = vars.map(&:to_i)
-  sum + x*y
+def part1()
+  program_memory = STDIN.read
+  valid_instructions = program_memory.scan(MUL_REGEX)
+  result = valid_instructions.inject(0) do |sum, ins|
+    vars = VARS_REGEX.match(ins).captures
+    x, y = vars.map(&:to_i)
+    sum + x*y
+  end
+  puts result
 end
 
-puts result
+INS_REGEX = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/
+
+def part2()
+  program_memory = STDIN.read
+  valid_instructions = program_memory.scan(INS_REGEX)
+  enabled = true
+  result = valid_instructions.inject(0) do |sum, ins|
+    case ins
+    when "do()"
+      enabled = true
+      sum
+    when "don't()"
+      enabled = false
+      sum
+    else # "mul(..)"
+      next sum unless enabled
+      vars = VARS_REGEX.match(ins).captures
+      x, y = vars.map(&:to_i)
+      sum + x*y
+    end
+  end
+  puts result
+end
+
+part2()
